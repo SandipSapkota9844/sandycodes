2022df152ccd7d8e4f3fa5bc0821761565c3ec72
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-01-12 11:50:43 +0545

    again

--- home/urls.py
+++ home/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
      path('', views.home, name="home"),
      path('contact', views.contact, name="contact"),
      path('about', views.about, name="about"),
+     path('search', views.search, name="search"),
+     path('signup', views.handleSignUp, name="handleSignUp"),
 ]
--- home/views.py
+++ home/views.py
@@ -2,6 +2,8 @@ from django.contrib import messages
 from django.shortcuts import HttpResponse, render
 from home.models import Contact
 from django.contrib import messages
+from blog.models import Post
+from django.contrib.auth.models import User
 
 
 def home(request):
@@ -12,15 +14,51 @@ def about(request):
     return render(request, 'home/about.html')
 
 
-from home.models import Contact
-
 def contact(request):
-    if request.method=="POST":
-        name=request.POST['name']
-        email=request.POST['email']
-        phone=request.POST['phone']
-        message =request.POST['content']
-        contact=Contact(name=name, email=email, phone=phone, message=message)
+    if request.method == "POST":
+        name = request.POST['name']
+        email = request.POST['email']
+        phone = request.POST['phone']
+        message = request.POST['content']
+        contact = Contact(name=name, email=email, phone=phone, message=message)
         contact.save()
     return render(request, "home/contact.html")
 
+
+def search(request):
+    query = request.GET['query']
+    if len(query) > 78:
+        allPosts = Post.objects.none()
+    else:
+        allPostsTitle = Post.objects.filter(title__icontains=query)
+        allPostsAuthor = Post.objects.filter(author__icontains=query)
+        allPostsContent = Post.objects.filter(content__icontains=query)
+        allPosts = allPostsTitle.union(allPostsContent, allPostsAuthor)
+    if allPosts.count() == 0:
+        messages.warning(
+            request, "No search results found. Please refine your query.")
+    params = {'allPosts': allPosts, 'query': query}
+    return render(request, 'home/search.html', params)
+
+def handleSignUp(request):
+    if request.method == "POST":
+        # Get the post parameters
+        username = request.POST['username']
+        email = request.POST['email']
+        fname = request.POST['fname']
+        lname = request.POST['lname']
+        pass1 = request.POST['pass1']
+        pass2 = request.POST['pass2']
+
+        # check for errorneous input
+
+        # Create the user
+        myuser = User.objects.create_user(username, email, pass1)
+        myuser.first_name = fname
+        myuser.last_name = lname
+        myuser.save()
+        messages.success(request, " Your iCoder has been successfully created")
+        return redirect('home')
+
+    else:
+      return HttpResponse("404 - Not found")
