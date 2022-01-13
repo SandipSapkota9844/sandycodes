7a16bee65a02f30a76ee4da18152cc83a0e95776
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-01-13 21:37:18 +0545

    admin theme added

--- Sandip/urls.py
+++ Sandip/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+admin.site.site_header="SandyCodes Admin"
+admin.site.site_title="SandyCodes Admin"
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
--- home/views.py
+++ home/views.py
@@ -1,10 +1,11 @@
+from django.shortcuts import redirect
 from django.contrib import messages
-from django.shortcuts import HttpResponse, render,redirect
+from django.shortcuts import HttpResponse, render, redirect
 from home.models import Contact
 from django.contrib import messages
 from blog.models import Post
 from django.contrib.auth.models import User
-
+from django.contrib.auth import authenticate,  login, logout
 
 
 def home(request):
@@ -15,6 +16,7 @@ def about(request):
     return render(request, 'home/about.html')
 
 
+# contact system starts here
 def contact(request):
     if request.method == "POST":
         name = request.POST['name']
@@ -26,6 +28,7 @@ def contact(request):
     return render(request, "home/contact.html")
 
 
+# search system starts here
 def search(request):
     query = request.GET['query']
     if len(query) > 78:
@@ -41,6 +44,8 @@ def search(request):
     params = {'allPosts': allPosts, 'query': query}
     return render(request, 'home/search.html', params)
 
+
+# signup fuctions starts here
 def handleSignUp(request):
     if request.method == "POST":
         # Get the post parameters
@@ -52,15 +57,54 @@ def handleSignUp(request):
         pass2 = request.POST['pass2']
 
         # check for errorneous input
+        if len(username) < 10:
+            messages.error(
+                request, " Your user name must be under 10 characters")
+            return redirect('home')
+
+        if not username.isalnum():
+            messages.error(
+                request, " User name should only contain letters and numbers")
+            return redirect('home')
+        if (pass1 != pass2):
+            messages.error(request, " Passwords do not match")
+            return redirect('home')
 
         # Create the user
         myuser = User.objects.create_user(username, email, pass1)
         myuser.first_name = fname
         myuser.last_name = lname
         myuser.save()
-        messages.success(request, " Your account has been successfully created")
+        messages.success(
+            request, " Your account has been successfully created")
         return redirect('home')
 
     else:
-      return HttpResponse("404 - Not found")
+        return HttpResponse("404 - Not found")
 
+
+# login function starts here
+def handeLogin(request):
+    if request.method == "POST":
+        # Get the post parameters
+        loginusername = request.POST['loginusername']
+        loginpassword = request.POST['loginpassword']
+
+        user = authenticate(username=loginusername, password=loginpassword)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Successfully Logged In")
+            return redirect("home")
+        else:
+            messages.error(request, "Invalid credentials! Please try again")
+            return redirect("home")
+
+    return HttpResponse("404- Not found")
+
+    return HttpResponse("login")
+
+
+def handelLogout(request):
+    logout(request)
+    messages.success(request, "Successfully logged out")
+    return redirect('home')
