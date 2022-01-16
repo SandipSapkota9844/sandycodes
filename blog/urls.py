b1ddfe802bcdd05312c5a224d72485a6ced78ef1
Sandip Sapkota <sandipsapkota9844@gmail.com>
2022-01-16 16:30:30 +0545

    alterd

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('', views.blogHome, name="blogHome"),
-    path('postComment', views.postComment, name="postComment"),
+     path('postComment', views.postComment, name="postComment"),
+    path('', views.blogHome, name="bloghome"),
     path('<str:slug>', views.blogPost, name="blogPost"),
-
 ]
